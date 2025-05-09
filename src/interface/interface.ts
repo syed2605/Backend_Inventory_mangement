3c51af1e1317c39d46f1ecb5eb715537366fa17f
Nitin Momaya <nitin.momaya@hcltech.com>
2025-05-09 11:27:08 +0530

    login setup done

---
+++ src/interface/interface.ts
@@ -0,0 +1,61 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IToken extends Document {
+    _id: mongoose.Schema.Types.ObjectId;
+     userId: string;
+     accessToken: string;
+     refreshToken: string;
+     accessTokenExpiry: Date;
+     refreshTokenExpiry: Date;
+     createdAt?: Date;
+    }
+    export interface UserDocument extends Document {
+        _id:mongoose.Schema.Types.ObjectId;
+          name?: string;
+          email?: string;
+          phone?: number;
+          password?: string;
+          createdAt?: Date;
+          updatedAt?: Date;
+        }
+    export interface UserService {
+        findUserByEmail(email: string, password: string): Promise<UserDocument | null>;
+        getUserById(userId: mongoose.Schema.Types.ObjectId): Promise<UserDocument | null>; // Added getUserById
+        createUser(user: UserDocument): Promise<UserDocument | null>; // Added getUserById
+        getUserByID(uid: mongoose.Schema.Types.ObjectId): Promise<UserDocument[] | null>; // Added getUserById
+      }
+      
+      export interface TokenService {
+        generateAccessToken(user: UserDocument): string;
+        generateRefreshToken(user: UserDocument): string;
+        findTokenByUserId(userId: mongoose.Schema.Types.ObjectId): Promise<IToken | null>;
+        createToken(
+          userId: mongoose.Schema.Types.ObjectId,
+          accessToken: string,
+          refreshToken: string,
+          accessTokenExpiry: Date,
+          refreshTokenExpiry: Date
+        ): Promise<IToken>;
+        updateToken(
+          tokenDocument: IToken, // Changed from refreshTokenData to tokenId, more generic
+          accessToken: string,
+          accessTokenExpiry: Date
+        ): Promise<IToken>;
+        deleteToken(tokenId: mongoose.Schema.Types.ObjectId): Promise<IToken | null>;
+      }   
+
+      export interface UserService {
+        findUserByEmail(email: string, password: string): Promise<UserDocument | null>;
+        getUserById(userId: mongoose.Schema.Types.ObjectId): Promise<UserDocument | null>; // Added getUserById
+        createUser(user: UserDocument): Promise<UserDocument | null>; // Added getUserById
+        getUserByID(uid: mongoose.Schema.Types.ObjectId): Promise<UserDocument[] | null>; // Added getUserById
+      }
+      export interface TokenDocument {
+        _id: mongoose.Schema.Types.ObjectId;
+        userId: string;
+        accessToken: string;
+        refreshToken: string;
+        accessTokenExpiry: Date;
+        refreshTokenExpiry: Date;
+        // Add other token properties as necessary
+      }
